1fb3666f9bdae8a89804389051af8e0dc5fd8364
Joe Jarvis <joe.jarvis109@gmail.com>
2017-06-20 23:33:54 -0400

    Support for sidebar. Crashes browser under certain conditions.

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,10 @@
-browser.storage.local.get("url").then((res) => {
+browser.storage.local.get().then((res) => {
     var url = res.url || "https://wikipedia.org";
+    var urlSidebar = res.urlSidebar || "https://wikipedia.org";
     browser.browserAction.setPopup({
         popup: url
+    });
+    browser.sidebarAction.setPanel({
+        panel: urlSidebar
     });
 });
--- extension/options.js
+++ extension/options.js
@@ -1,17 +1,24 @@
 function saveOptions(e) {
     var url = document.querySelector("#url").value || "https://wikipedia.org";
+    var urlSidebar = document.querySelector("#urlSidebar").value || "https://wikipedia.org";
     browser.storage.local.set({
-        url: url
+        url: url,
+        urlSidebar: urlSidebar
     });
     browser.browserAction.setPopup({
         popup: url
     });
+    browser.sidebarAction.setPanel({
+        panel: urlSidebar
+    });
     e.preventDefault();
 }
 function restoreOptions() {
-    browser.storage.local.get("url").then((res) => {
+    browser.storage.local.get().then((res) => {
         document.querySelector("#url").value = res.url || "https://wikipedia.org";
+        document.querySelector("#urlSidebar").value = res.urlSidebar || "https://wikipedia.org";
     });
 }
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.getElementById("url").addEventListener("input", saveOptions);
+document.getElementById("url").addEventListener("change", saveOptions);
+document.getElementById("urlSidebar").addEventListener("change", saveOptions);
